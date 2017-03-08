c3460fe82e285ac22f2585f5589166b7f955e826
Nils Larsgård <nilsmagnus@gmail.com>
2017-03-08 17:47:23 +0100

    Add dists for ubuntu, win, darwin

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # serve
 Serve static files on http from command line
+
+# Usage
+
+    # serve current folder on port 8080
+    serve 
+
+    # serve on custom port and folder
+    serve -port 1234 -folder /var/www
+    
+    # display help
+    serve -h
+
+# Install
+    
+    go get github.com/nilsmagnus/serve
+   
+Or download binary:
+
+- [For ubuntu](dist/ubuntu/serve)
+- [For darwin](dist/darwin/serve)
+- [For windows](dist/win/serve.exe)
+
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"flag"
+	"fmt"
+)
+
+var port int
+var folder string
+
+func main() {
+
+	currentDir, _ := os.Getwd()
+
+	flag.IntVar(&port, "port", 8080, "http port")
+	flag.StringVar(&folder, "folder", currentDir, "folder to serve, defaults to current")
+	flag.Parse()
+
+	fmt.Printf("Serving %v on port %d\n", folder, port)
+	fmt.Println("Press ctrl-c to end")
+
+	http.Handle("/", http.FileServer(http.Dir(currentDir)))
+
+	http.ListenAndServe(":8081", nil)
+}
